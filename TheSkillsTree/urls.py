858d80898ce20c6e30c34c6e4518d0067696a6aa
pawan kumar gond <98225403+kpawangond@users.noreply.github.com>
2025-08-22 17:03:53 +0530

    Update urls.py

--- TheSkillsTree/urls.py
+++ TheSkillsTree/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('skills.urls')),
 ]
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
